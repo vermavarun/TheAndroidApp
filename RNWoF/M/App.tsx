4b6033d39bd984121e63bd9e66240e1482c59813
Varun Verma <vermavarun@outlook.com>
2026-02-27 18:26:52 +0530

    feat: implement CalculatorBasic component with basic arithmetic operations and alert functionality

--- RNWoF/M/App.tsx
+++ RNWoF/M/App.tsx
@@ -5,12 +5,12 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import CalculatorBasic from './components/CalculatorBasic';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,10 +28,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <CalculatorBasic />
     </View>
   );
 }
